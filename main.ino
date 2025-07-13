87e6696decdc70b1bf302d287a5547f2d05caacb
joshuaglenen <joshglenen1@gmail.com>
2025-07-13 20:39:40 -0300

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,10 +1,13 @@
 #include <SD_MMC.h>
 
-#define micPin 36
-#define pushButton 23
-#define rgb_r 18
-#define rgb_g 19
-#define rgb_b 21
+#define micPin 4
+#define pushButton 47
+#define rgb_r 37
+#define rgb_g 36
+#define rgb_b 35
+#define SD_MMC_CMD 38 //Please do not modify it.
+#define SD_MMC_CLK 39 //Please do not modify it. 
+#define SD_MMC_D0  40 //Please do not modify it.
 
 const uint32_t MAX_FILE_SIZE = 950UL * 1024UL * 1024UL;  // 950 MB limit
 File audioFile;
@@ -22,15 +25,30 @@ void setup() {
   pinMode(rgb_b, OUTPUT);
   pinMode(pushButton, INPUT_PULLUP);
 
-  if (!SD_MMC.begin()) {
+  //detect sd card and availible space
+  SD_MMC.setPins(SD_MMC_CLK, SD_MMC_CMD, SD_MMC_D0);
+  if (!SD_MMC.begin("/sdcard", true, true, SDMMC_FREQ_DEFAULT, 5)) {
     Serial.println("SD Card Mount Failed");
     rgb(true, false, false);  // Red
     while (true);
   }
-
+  uint8_t cardType = SD_MMC.cardType();
+  if(cardType == CARD_NONE){
+      Serial.println("No SD_MMC card attached");
+      return;
+  }
+  Serial.print("SD_MMC Card Type: ");
+  if(cardType == CARD_MMC){
+      Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+      Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+      Serial.println("SDHC");
+  } else {
+      Serial.println("UNKNOWN");
+  }
   uint64_t maxSpace = SD_MMC.totalBytes();
   uint64_t usedSpace = SD_MMC.usedBytes();
-  
   if(0.01>(float)(maxSpace-usedSpace)/(float)maxSpace)
   {
     full = true;
